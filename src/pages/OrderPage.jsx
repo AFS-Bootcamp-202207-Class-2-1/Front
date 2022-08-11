6b5d3f4ba2b61a4d97bb79a5f6322ffd5d7fe7d4
Niko <NIKO.Z.H.WANG@OOCL.COM>
2022-08-11 15:51:21 +0800

    feat: implement comment function'

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Card, Row, Col, Modal } from "antd";
+import { Card, Row, Col, Modal, Button, Input } from "antd";
 import "../assets/less/orderPage.less";
 import { useEffect } from "react";
 import { getTicketInfo, deleteTicket } from "../api/ticketInfo";
@@ -8,6 +8,27 @@ import { DeleteOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
 export default function OrderPage() {
   const { confirm } = Modal;
   const [ticketInfo, setTicketInfo] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState("Content of the modal");
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setModalText("The modal will be closed after two seconds");
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setVisible(false);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -99,14 +120,25 @@ export default function OrderPage() {
                 <Col className="gutter-row" span={6}>
                   <div className="price">¥{ticketInfo[index].ticketPrice}</div>
                 </Col>
-                {/* <Col className="gutter-row" span={6}>
-                  <div className="status">col-6</div>
-                </Col> */}
+                <Col className="gutter-row" span={6}>
+                  <Button type="primary" onClick={showModal}>
+                    Comment
+                  </Button>
+                </Col>
               </Row>
             </Card>
           );
         })}
       </Card>
+      <Modal
+        title="Comment"
+        visible={visible}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <Input placeholder="Please input your comment" />
+      </Modal>
     </div>
   );
 }
