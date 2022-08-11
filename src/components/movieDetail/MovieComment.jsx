7fda58b516cdc604ab20052e93e223f92ecad237
Niko <NIKO.Z.H.WANG@OOCL.COM>
2022-08-11 22:25:26 +0800

    feat: implement comment function

--- src/components/movieDetail/MovieComment.jsx
+++ src/components/movieDetail/MovieComment.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 // import detail_comment_icon from '../assets/iconfont/detail_comment_icon.png'
-import MovieBanner from './MovieBanner';
+import MovieBanner from "./MovieBanner";
+import { Comment, List, Tooltip } from "antd";
+import moment from "moment";
+import "../../assets/less/movieComment.less";
+import boy from "../../assets/images/boy.png";
+import girl from "../../assets/images/girl.png";
 
-const MovieComment = () => {
-    return (
-        <div>
-            <MovieBanner title="观影评论" icon="../assets/iconfont/detail_comment_icon.png"/>
-        </div>
-    );
+const MovieComment = (props) => {
+  const { comments } = props;
+  console.log(comments);
+
+  return (
+    <div className="comment">
+      <MovieBanner
+        title="观影评论"
+        icon="../assets/iconfont/detail_comment_icon.png"
+      />
+      <List
+        className="comment-list"
+        // header={`${comments.length} replies`}
+        itemLayout="horizontal"
+        dataSource={comments}
+        renderItem={(item) => (
+          <li>
+            <Comment
+              author={item.usersName}
+              avatar={
+                item.usersGender === "male" ? (
+                  <img src={boy} with="32px" height="32px"></img>
+                ) : (
+                  <img src={girl} with="32px" height="32px"></img>
+                )
+              }
+              content={item.commentContent}
+              datetime={item.commentTime}
+            />
+          </li>
+        )}
+      />
+    </div>
+  );
 };
 
 export default MovieComment;
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,32 +1,30 @@
-import React ,{ useState,useEffect }from 'react'
-import MovieDetails from '../components/movieDetail/MovieDetails'
-import '../assets/less/MoviesPage.less'
-import MovieIntroduce from '../components/movieDetail/MovieIntroduce'
-import MovieImages from '../components/movieDetail/MovieImages'
-import MovieComment from '../components/movieDetail/MovieComment'
-import {getMovieDetail} from "../api/ticketSelect"
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import MovieDetails from "../components/movieDetail/MovieDetails";
+import "../assets/less/MoviesPage.less";
+import MovieIntroduce from "../components/movieDetail/MovieIntroduce";
+import MovieImages from "../components/movieDetail/MovieImages";
+import MovieComment from "../components/movieDetail/MovieComment";
+import { getMovieDetail } from "../api/ticketSelect";
+import { useParams } from "react-router-dom";
 
-function MoviesPage(props){
+function MoviesPage(props) {
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
 
-    const [movie, setMovie] = useState([]);
-    const { id } = useParams();
-    
-    useEffect(() => {
-        getMovieDetail(id).then((response) => {
-            setMovie(response.data)
-        })
-    },[])
+  useEffect(() => {
+    getMovieDetail(id).then((response) => {
+      setMovie(response.data);
+    });
+  }, []);
 
-
-    return (
-        <div>
-            <MovieDetails movie={movie} />
-            <MovieIntroduce movie={movie}/>
-            <MovieImages movie={movie}/>
-            <MovieComment/>
-        </div>
-    )
+  return (
+    <div>
+      <MovieDetails movie={movie} />
+      <MovieIntroduce movie={movie} />
+      <MovieImages movie={movie} />
+      <MovieComment comments={movie.comments} />
+    </div>
+  );
 }
 
-export default MoviesPage
+export default MoviesPage;
